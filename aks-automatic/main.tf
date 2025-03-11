1f0c48c016d63931d6e34ea40a540c4dd6bf619f
Russell de Pina <rdepina@microsoft.com>
2025-03-11 14:37:04 -0500

    add managed prom and graf to deployment

--- aks-automatic/main.tf
+++ aks-automatic/main.tf
@@ -17,7 +17,7 @@ resource "tls_private_key" "ssh_key" {
 }
 
 resource "azapi_resource" "aks_auto" {
-  type      = "Microsoft.ContainerService/managedClusters@2024-10-02-preview"
+  type      = "Microsoft.ContainerService/managedClusters@2024-06-02-preview"
   name      = "aks-${var.project_prefix}-${random_id.suffix.id}"
   parent_id = azurerm_resource_group.rg.id
   location  = var.location
@@ -56,8 +56,8 @@ resource "azapi_resource" "aks_auto" {
         metrics = {
           enabled = true
           kubeStateMetrics = {
-            metricsAnnotationsAllowList = var.ksm_allowed_annotations
-            metricsLabelsAllowList = var.ksm_allowed_labels
+            metricAnnotationsAllowList = var.ksm_allowed_annotations
+            metricLabelsAllowlist = var.ksm_allowed_labels
           }
         }
       }
@@ -74,8 +74,58 @@ resource "azapi_resource" "aks_auto" {
   })
 }
 
+resource "azurerm_monitor_workspace" "amw" {
+  name                = "amon-${var.project_prefix}-${random_id.suffix.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_monitor_data_collection_endpoint" "prom_endpoint" {
+  name                = "prom-${var.project_prefix}-${random_id.suffix.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind                = "Linux"
+}
+
+resource "azurerm_monitor_data_collection_rule" "azprom_dcr" {
+  name                        = "promdcr-${var.project_prefix}-${random_id.suffix.id}"
+  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg.location
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.prom_endpoint.id
+
+  data_sources {
+    prometheus_forwarder {
+      name    = "PrometheusDataSource"
+      streams = ["Microsoft-PrometheusMetrics"]
+    }
+  }
+
+  destinations {
+    monitor_account {
+      monitor_account_id = azurerm_monitor_workspace.amw.id
+      name               = azurerm_monitor_workspace.amw.name
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-PrometheusMetrics"]
+    destinations = [azurerm_monitor_workspace.amw.name]
+  }
+}
+
+# associate to a Data Collection Rule
+resource "azurerm_monitor_data_collection_rule_association" "example_dcr_to_aks" {
+  name                    = "dcr-${azapi_resource.aks_auto.name}"
+  target_resource_id      = azapi_resource.aks_auto.id
+  data_collection_rule_id = azurerm_monitor_data_collection_rule.azprom_dcr.id
+}
+
+# associate to a Data Collection Endpoint
+resource "azurerm_monitor_data_collection_rule_association" "example_dce_to_aks" {
+  target_resource_id          = azapi_resource.aks_auto.id
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.prom_endpoint.id
+}
 
 
 
 data "azurerm_client_config" "current" {}
-
