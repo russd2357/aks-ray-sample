c5799dcc487baa84e17c0982eb4bfc0606f839f2
Russell de Pina <rdepina@microsoft.com>
2025-03-19 09:54:44 -0500

    changes to add Managed Prom and Graf

--- aks-automatic/main.tf
+++ aks-automatic/main.tf
@@ -1,9 +1,11 @@
-resource "random_id" "suffix" {
-  byte_length = 6
+resource "random_string" "suffix" {
+  length = 6
+  special = false
+  upper = false
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.project_prefix}-${random_id.suffix.id}"
+  name     = "rg-${var.project_prefix}-${random_string.suffix.result}"
   location = var.location
 
   tags = {
@@ -18,7 +20,7 @@ resource "tls_private_key" "ssh_key" {
 
 resource "azapi_resource" "aks_auto" {
   type      = "Microsoft.ContainerService/managedClusters@2024-06-02-preview"
-  name      = "aks-${var.project_prefix}-${random_id.suffix.id}"
+  name      = "aks-${var.project_prefix}-${random_string.suffix.result}"
   parent_id = azurerm_resource_group.rg.id
   location  = var.location
   tags     = azurerm_resource_group.rg.tags
@@ -27,7 +29,7 @@ resource "azapi_resource" "aks_auto" {
 
     properties = {
       kubernetesVersion = "1.31"
-      nodeResourceGroup = "MC-aks-${var.project_prefix}-${random_id.suffix.id}"
+      nodeResourceGroup = "MC-aks-${var.project_prefix}-${random_string.suffix.result}"
       agentPoolProfiles = [
         {
           name    = "systempool"
@@ -36,7 +38,7 @@ resource "azapi_resource" "aks_auto" {
           tags    = { owner = var.resource_group_owner }
           mode    = "System"
           osType  = "Linux"
-          osSKU = "AzureLinux"
+          osSKU   = "AzureLinux"
           osDiskSizeGB = 64
           enableAutoScaling = false
         }
@@ -74,21 +76,71 @@ resource "azapi_resource" "aks_auto" {
   })
 }
 
+resource "null_resource" "wait_for_aks" {
+  depends_on = [azapi_resource.aks_auto]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      max_retries=10
+      retries=0
+      while [ "$(az aks show --resource-group ${azurerm_resource_group.rg.name} --name ${azapi_resource.aks_auto.name} --query "provisioningState" -o tsv)" != "Succeeded" ]; do
+        if [ $retries -ge $max_retries ]; then
+          echo "Max retries exceeded. Exiting..."
+          exit 1
+        fi
+        echo "Waiting for AKS cluster to be fully provisioned... (Attempt: $((retries+1)))"
+        retries=$((retries+1))
+        sleep 30
+      done
+    EOT
+  }
+}
+
+resource "azapi_update_resource" "k8s-default-node-pool-systempool-taint" {
+  type        = "Microsoft.ContainerService/managedClusters@2024-06-02-preview"
+  resource_id = azapi_resource.aks_auto.id
+  body = jsonencode({
+    properties = {
+      agentPoolProfiles = [
+        {
+          name = "systempool"
+          nodeTaints = ["CriticalAddonsOnly=true:NoSchedule"]
+        }
+      ]
+    }
+  })
+
+  depends_on = [null_resource.wait_for_aks]
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "workload" {
+  name                  = "ray"
+  kubernetes_cluster_id = azapi_resource.aks_auto.id
+  vm_size               = var.ray_node_pool_vm_size
+  node_count            = 4
+  os_type               = "Linux"
+  os_sku                = "AzureLinux" 
+  os_disk_type          = "Ephemeral"
+  mode                  = "User"
+
+  depends_on = [azapi_update_resource.k8s-default-node-pool-systempool-taint]
+}
+
 resource "azurerm_monitor_workspace" "amw" {
-  name                = "amon-${var.project_prefix}-${random_id.suffix.id}"
+  name                = "amon-${var.project_prefix}-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_monitor_data_collection_endpoint" "prom_endpoint" {
-  name                = "prom-${var.project_prefix}-${random_id.suffix.id}"
+  name                = "prom-${var.project_prefix}-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "Linux"
 }
 
 resource "azurerm_monitor_data_collection_rule" "azprom_dcr" {
-  name                        = "promdcr-${var.project_prefix}-${random_id.suffix.id}"
+  name                        = "promdcr-${var.project_prefix}-${random_string.suffix.result}"
   resource_group_name         = azurerm_resource_group.rg.name
   location                    = azurerm_resource_group.rg.location
   data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.prom_endpoint.id
@@ -126,6 +178,28 @@ resource "azurerm_monitor_data_collection_rule_association" "example_dce_to_aks"
   data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.prom_endpoint.id
 }
 
+# add managed grafana
+resource "azurerm_dashboard_grafana" "graf" {
+  name                = "graf-${var.project_prefix}-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  api_key_enabled     = true
+  grafana_major_version = "10"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_monitor_workspace_integrations {
+    resource_id = azurerm_monitor_workspace.amw.id
+  }
+}
+
+resource "azurerm_role_assignment" "graf_role" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.graf.identity[0].principal_id
+}
 
 
 data "azurerm_client_config" "current" {}
--- aks-automatic/outputs.tf
+++ aks-automatic/outputs.tf
@@ -1,3 +1,17 @@
 output aks_cluster_name {
   value = azapi_resource.aks_auto.name
 }
+
+output resource_group_name {
+  value = azurerm_resource_group.rg.name
+}
+
+output azure_monitor_workspace_name {
+  value = azurerm_monitor_workspace.amw.name
+}
+
+output grafana_dashboard_name {
+  value = azurerm_dashboard_grafana.graf.name
+}
+
+
