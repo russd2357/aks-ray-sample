2628c98e1e49a512c1220da0c6be30a8e28468a3
Russell de Pina <rdepina@microsoft.com>
2025-04-03 13:55:16 -0500

    add outputs for data collection rule and endpoints

--- aks-automatic/outputs.tf
+++ aks-automatic/outputs.tf
@@ -14,4 +14,10 @@ output grafana_dashboard_name {
   value = azurerm_dashboard_grafana.graf.name
 }
 
+output data_collection_rule_id {
+  value = azurerm_monitor_data_collection_rule.azprom_dcr.immutable_id
+}
 
+output data_collection_endpoint_url {
+  value = azurerm_monitor_data_collection_endpoint.prom_endpoint.logs_ingestion_endpoint
+}
